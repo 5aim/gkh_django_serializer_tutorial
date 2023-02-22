93b14299bbd36d7f65afc5d56c1b122185cb9dd3
김혁진 <hyukjin720@gmail.com>
2023-02-22 17:35:25 +0900

    articles serializer post

--- articles/views.py
+++ articles/views.py
@@ -5,8 +5,12 @@ from articles.serializers import ArticleSerializer
 from articles.models import Article
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def index(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        print(request.data['key'])
+        return Response()
